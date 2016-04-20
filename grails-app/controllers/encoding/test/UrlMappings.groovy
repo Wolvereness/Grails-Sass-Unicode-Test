2f36dc7060d29de7d73c2e308678720de4124b7a
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2016-04-19 23:49:10 -0500

    Grails, I can has sassy quotes?

---
+++ grails-app/controllers/encoding/test/UrlMappings.groovy
@@ -0,0 +1,15 @@
+package encoding.test
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "404"(view:'/index')
+    }
+}
---
+++ grails-app/views/index.gsp
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <title>Welcome to Grails</title>
+	<asset:stylesheet src="index.css"/>
+</head>
+<body>
+	<span class="quotes">
+		This is a quote test.
+    </span>
+	<br />
+	<span class="quote">
+		This is an imported quote test.
+    </span>
+</body>
+</html>
